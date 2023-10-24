85ca367f1153871b5ca3ca2789f993d21c1f425d
vanilnic <bikadog306@gmail.com>
2023-10-24 21:59:37 +0300

    Добавлен скроллинг страницы

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// const htmlEl = document.getElementsByTagName('html')[0];
-
-// const toggleTheme = (theme) => {
-//     htmlEl.dataset.theme = theme;
-// }
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+   
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+  });
