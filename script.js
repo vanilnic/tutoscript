6268dbf2cdb4e85284c0dccdecaa5c9d9b8554b2
vanilnic <bikadog306@gmail.com>
2023-10-27 22:33:47 +0300

    преддебют

--- script.js
+++ script.js
@@ -10,7 +10,38 @@ $('a[href*="#"]').on('click', function (e) {
 function setStyleSheet(url){
   var stylesheet = document.getElementById("stylesheet");
   stylesheet.setAttribute('href', url);
+  // frames['frr'].document.head.appendChild(cssLink);
 }
 document.getElementsByTagName('head')(0).getElementById('stylesheet').href='stylesheet2.css';
 document.getElementsByTagName('menu.html')(0).getElementById('stylesheet').href='stylesheet2.css';
-document.getElementsByTagName('frr')(0).getElementById('stylesheet').href='stylesheet2.css';
+// document.getElementsByTagName('frr')(0).getElementById('stylesheet').href='stylesheet2.css';
+
+function setStyleSheet_l(url){
+  var cssLink = document.createElement("link");
+  cssLink.href = "index.css"; 
+  cssLink.rel = "stylesheet"; 
+  cssLink.type = "text/css"; 
+  frames['frr'].document.head.appendChild(cssLink);
+}
+function setStyleSheet_d(url){
+  var cssLink = document.createElement("link");
+  cssLink.href = "index_dark.css"; 
+  cssLink.rel = "stylesheet"; 
+  cssLink.type = "text/css"; 
+  frames['frr'].document.head.appendChild(cssLink);
+}
+
+function setStyleSheet_ll(url){
+  var cssLink = document.createElement("link");
+  cssLink.href = "index.css"; 
+  cssLink.rel = "stylesheet"; 
+  cssLink.type = "text/css"; 
+  frames['fr'].document.head.appendChild(cssLink);
+}
+function setStyleSheet_dd(url){
+  var cssLink = document.createElement("link");
+  cssLink.href = "index_dark.css"; 
+  cssLink.rel = "stylesheet"; 
+  cssLink.type = "text/css"; 
+  frames['fr'].document.head.appendChild(cssLink);
+}
