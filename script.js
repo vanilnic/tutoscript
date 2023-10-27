160ae6d53f75f770678976fb47255ed7cb54d6a1
vanilnic <bikadog306@gmail.com>
2023-10-27 20:16:02 +0300

    Визуальные изменения

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ $('a[href*="#"]').on('click', function (e) {
       scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
   });
+
+  
+function setStyleSheet(url){
+  var stylesheet = document.getElementById("stylesheet");
+  stylesheet.setAttribute('href', url);
+}
+document.getElementsByTagName('head')(0).getElementById('stylesheet').href='stylesheet2.css';
+document.getElementsByTagName('menu.html')(0).getElementById('stylesheet').href='stylesheet2.css';
+document.getElementsByTagName('frr')(0).getElementById('stylesheet').href='stylesheet2.css';
